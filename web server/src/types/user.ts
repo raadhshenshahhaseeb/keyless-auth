5c2cc7ccb78a3d5a160e03b6b54091ef2df5c41b
Soumyadeep <soumyadeeproy.csbs@gmail.com>
2025-03-03 19:34:05 +0530

    user.ts modified by adding mail

--- web server/src/types/user.ts
+++ web server/src/types/user.ts
@@ -1,7 +1,7 @@
 export interface User {
     id?: number;
     google_id?: string;
-    email: string;            // Added required email field
+    email: string;           
     name: string;
     profile_picture?: string;
     created_at?: Date;
