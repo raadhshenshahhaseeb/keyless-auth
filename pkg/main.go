e160b9acecc777b99ca3cf8a49413a6d78cadd3d
Diego Gallegos <garndieg@amazon.com>
2025-03-05 20:45:35 +0000

    set env variables

--- pkg/main.go
+++ pkg/main.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"keyless-auth/api"
 	"keyless-auth/repository"
 	"keyless-auth/storage"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/redis/go-redis/v9"
 )
 
+var (
+	redisHost     string
+	redisPort     string
+	redisPassword string
+	redisUsername string
+)
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
+
+func init() {
+	redisHost = getEnvOrDefault("REDIS_HOST", "localhost")
+	redisPort = getEnvOrDefault("REDIS_PORT", "6379")
+	redisPassword = getEnvOrDefault("REDIS_PASSWORD", "")
+	redisUsername = getEnvOrDefault("REDIS_USERNAME", "")
+}
+
 func main() {
 	db, err := storage.NewRedisClient(context.Background(), &redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Username: redisUsername,
+		Addr:     redisHost + ":" + redisPort,
+		Password: redisPassword,
 		DB:       0,
 	})
 
@@ -33,4 +59,8 @@ func main() {
 	router.HandleFunc("/credentials/{credential}", credentialsHandler.GetWalletAddressByCredential).Methods("GET")
 	router.HandleFunc("/credentials", credentialsHandler.GenerateCredential).Methods("POST")
 	router.HandleFunc("/proof", proofHandler.GenerateProof).Methods("POST")
+
+	serverAddr := fmt.Sprintf(":%s", getEnvOrDefault("APP_PORT", "8080"))
+	log.Printf("Server starting on %s", serverAddr)
+	log.Fatal(http.ListenAndServe(serverAddr, router))
 }
